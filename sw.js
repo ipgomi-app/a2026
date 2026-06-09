6e729cc50a445c212ab099cecaca6a089819785e
KPP <llljjjlll78@gmail.com>
2026-06-09 23:01:52 +0900

    SW v4: 활성화시 postMessage로 자동 새로고침, placeholder 영남 적용

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // KPP 운송견적 PWA 서비스워커
-const CACHE = 'kpp-quote-v3';
+const CACHE = 'kpp-quote-v4';
 const ASSETS = [
   './',
   './index.html',
@@ -24,6 +24,8 @@ self.addEventListener('activate', e => {
     caches.keys()
       .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
       .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then(clients => clients.forEach(c => c.postMessage({ type: 'SW_UPDATED' }))) // 모든 탭 리로드 신호
   );
 });
 
